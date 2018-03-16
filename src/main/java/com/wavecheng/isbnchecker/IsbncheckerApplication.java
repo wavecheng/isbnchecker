66f2f4200a62483c262d0255be7883699b52dc30
bo chen <bo.chen@citrix.com>
2018-03-16 21:53:50 +0800

    add activeMQ all

---
+++ src/main/java/com/wavecheng/isbnchecker/IsbncheckerApplication.java
@@ -0,0 +1,12 @@
+package com.wavecheng.isbnchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbncheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbncheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wavecheng/isbnchecker/IsbncheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wavecheng.isbnchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IsbncheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
